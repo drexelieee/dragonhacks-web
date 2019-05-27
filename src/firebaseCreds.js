39a22133d8a1bee58e71a83ac9b4f200a1739bca
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-05-27 01:47:35 -0400

    Remove secrets from repo

--- src/firebaseCreds.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAQAzPzS7SQqbyO-Tjg4WrtjApd7SMAH9M",
-  authDomain: "dragonhacks-web.firebaseapp.com",
-  databaseURL: "https://dragonhacks-web.firebaseio.com",
-  projectId: "dragonhacks-web",
-  storageBucket: "dragonhacks-web.appspot.com",
-  messagingSenderId: "287317729268",
-  appId: "1:287317729268:web:f427de4cfd2ab971"
-};
