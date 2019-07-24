8decb4b15618894e9854daf2ea9f672ebdda23fc
Jason R. Carrete <jasoncarrete5@gmail.com>
2019-07-24 17:54:27 -0400

    Fix code style

--- src/forms/SponsorForm.js
+++ src/forms/SponsorForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './SponsorForm.css'
 
 export default class SponsorForm extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       organization: '',
@@ -15,7 +15,7 @@ export default class SponsorForm extends Component {
     this.handlerFor = this.handlerFor.bind(this)
   }
 
-  handlerFor(valueName) {
+  handlerFor (valueName) {
     let handler = (event) => {
       this.setState({
         [valueName]: event.target.value
@@ -29,15 +29,15 @@ export default class SponsorForm extends Component {
       <form>
         <label>
           Organization Name
-          <input type='text' value={this.state.organization} onChange={this.handlerFor('organization')} required/>
+          <input type='text' value={this.state.organization} onChange={this.handlerFor('organization')} required />
         </label>
         <label>
           Name
-          <input type='text' value={this.state.contactName} onChange={this.handlerFor('contactName')} required/>
+          <input type='text' value={this.state.contactName} onChange={this.handlerFor('contactName')} required />
         </label>
         <label>
           Email
-          <input type='email' value={this.state.contactEmail} onChange={this.handlerFor('contactEmail')} required/>
+          <input type='email' value={this.state.contactEmail} onChange={this.handlerFor('contactEmail')} required />
         </label>
         <select value={this.state.package} onChange={this.handlerFor('package')}>
           <option value=''>Uncertain</option>
@@ -46,9 +46,9 @@ export default class SponsorForm extends Component {
           <option value='gold'>Gold</option>
           <option value='platinum'>Platinum</option>
         </select>
-        <label>Optional Donation<input type='number' value={this.state.donation} onChange={this.handlerFor('donation')}/></label>
-        <textarea value={this.state.message} onChange={this.handlerFor('message')}></textarea>
-        <input type='submit'/>
+        <label>Optional Donation<input type='number' value={this.state.donation} onChange={this.handlerFor('donation')} /></label>
+        <textarea value={this.state.message} onChange={this.handlerFor('message')} />
+        <input type='submit' />
       </form>
     )
   }
