4a236f91d45eac8277329ee7872fb180ac1cb3fb
Jason R. Carrete <jasoncarrete5@gmail.com>
2019-07-24 16:54:31 -0400

    Add theme.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './theme.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
