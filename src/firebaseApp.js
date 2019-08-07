2a085bdc2d02680cdf161f892f376c7199562ca2
Zach <zachmatuson@gmail.com>
2019-08-07 19:45:07 -0400

    added local function emulation on local builds

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -2,8 +2,9 @@ import firebase from 'firebase/app';
 import 'firebase/functions'
 import 'firebase/auth'
 import 'firebase/firestore'
+// import * as process from "process"
 
-export default firebase.initializeApp({
+let app = firebase.initializeApp({
   apiKey: 'AIzaSyAxS4fEOuTRiKbrQV7zd0B3_Zot0KZo_so',
   authDomain: 'dragonhacks-web-dev.firebaseapp.com',
   databaseURL: 'https://dragonhacks-web-dev.firebaseio.com',
@@ -11,4 +12,10 @@ export default firebase.initializeApp({
   storageBucket: 'dragonhacks-web-dev.appspot.com',
   messagingSenderId: '389172751187',
   appId: '1:389172751187:web:d2a2090416fb608b'
-})
+});
+
+if (process.env.REACT_APP_ENV === "development") {
+  app.functions().useFunctionsEmulator("http://localhost:5001");
+}
+
+export default app;
