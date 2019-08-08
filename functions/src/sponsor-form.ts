802f088ad469d9948453f519a181f6532cc08dad
Jason Carrete <jcarrete5@users.noreply.github.com>
2019-08-07 21:28:31 -0400

    Change " to '

--- functions/src/sponsor-form.ts
+++ functions/src/sponsor-form.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as nodemailer from "nodemailer";
+import * as nodemailer from 'nodemailer';
 import { MailOptions } from 'nodemailer/lib/json-transport';
 
 const transporter = nodemailer.createTransport({
