824d43d561d12ff8ffb2eec201e736fff70b1522
Jason R. Carrete <jasoncarrete5@gmail.com>
2019-08-07 21:22:47 -0400

    Style sponsor form with Material UI
    use prettier to format code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import SponsorForm from './forms/SponsorForm'
+// import { ThemeProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+
+// const theme = {
+//   palette: {
+
+//   },
+//   fontFamily: ['Roboto', 'sans-serif']
+// }
 
 function App() {
   return (
-    <div>
+    // <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <CssBaseline />
       <h2>Sponsor Us</h2>
       <SponsorForm />
-    </div>
+    </React.Fragment>
+    // </ThemeProvider>
   )
 }
 
