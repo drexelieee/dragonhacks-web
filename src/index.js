4784d211fae702e94e3c44b59f1411626f2d8f6b
Hoang Nguyen <mizore742@hotmail.com>
2019-08-11 16:55:20 -0400

    Added React Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import './theme.css'
+import Landing from './landing/Landing'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const routing = (
+	<Router>
+		<div>
+			<Route path="/" component={ Landing } />
+		</div>
+	</Router>
+	)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(routing, document.getElementById('root'))
 serviceWorker.unregister()
