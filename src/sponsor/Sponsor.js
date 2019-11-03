afda1c3892a9e54afcd3f6268986f2368b29a6a5
Zach <zachmatuson@gmail.com>
2019-11-03 11:32:56 -0500

    Moved stats to cards

--- src/sponsor/Sponsor.js
+++ src/sponsor/Sponsor.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect, useRef } from 'react'
 import app from '../firebaseApp'
 import SponsorForm from '../forms/SponsorForm'
 import styles from './Sponsor.module.css'
+import StatisticCard from '../statistics/StatisticCard'
+import StatisticCardRow from '../statistics/StatisticCardRow'
+import SchoolIcon from '@material-ui/icons/School'
+import ListAltIcon from '@material-ui/icons/ListAlt'
+import PersonIcon from '@material-ui/icons/Person'
 
 export default function Sponsor(props) {
   let [stats, setStats] = useState({})
@@ -22,17 +27,26 @@ export default function Sponsor(props) {
 
   return (
     <div className={styles.sponsorWrapper}>
-      <h1>Statistics from last year</h1>
-      <p className={styles.statistic}>
-        <strong>{stats.registeredCount} </strong>
-        registrations
-      </p>
-      <p className={styles.statistic}>
-        <strong>{stats.attendedCount} </strong>
-        participants from over
-        <strong> {stats.schoolCount} </strong>
-        different schools
-      </p>
+      <StatisticCardRow>
+        <StatisticCard
+          content={stats.registeredCount}
+          description='Registrations'
+          icon={ListAltIcon}
+          iconBg='#fd8783'
+        />
+        <StatisticCard
+          content={stats.attendedCount}
+          description='Participants'
+          icon={PersonIcon}
+          iconBg='#62d777'
+        />
+        <StatisticCard
+          content={stats.schoolCount}
+          description='Schools'
+          icon={SchoolIcon}
+          iconBg='#a782fd'
+        />
+      </StatisticCardRow>
       <SponsorForm />
     </div>
   )
---
+++ src/statistics/StatisticCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './StatisticCard.module.css'
+import { lighten } from '@material-ui/core/styles'
+
+export default function StatisticCard(props) {
+  const Icon = props.icon
+  return (
+    <div className={styles.statisticCard}>
+      <div
+        className={styles.statisticIconContainer}
+        style={{ backgroundColor: lighten(props.iconBg, 0.8) }}
+      >
+        <Icon style={{ color: props.iconBg }} />
+      </div>
+      <strong className={styles.statisticContent}>{props.content}</strong>
+      <div className={styles.statisticDescription}>{props.description}</div>
+    </div>
+  )
+}
