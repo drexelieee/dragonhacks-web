b1258bb72cce8110c914f7873356a7306d66c14d
Steven Kearns <skearns@vgimfs.com>
2019-11-10 12:48:23 -0500

    Add more images and website masters section

--- src/about/About.js
+++ src/about/About.js
@@ -1,37 +1,67 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { makeStyles } from '@material-ui/core/styles'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridListTileBar from '@material-ui/core/GridListTileBar'
 import ListSubheader from '@material-ui/core/ListSubheader'
-import execBoardData from './teamNames.js'
-import styles from './About.module.css'
+import * as teamNames from './teamNames.js'
+// import styles from './About.module.css'
 // import MdcImageList from '@material/image-list/mdc-image-list'
 
-export default class About extends React.Component {
-  render() {
-    return (
-      <div className={styles.root}>
-        <h3>
-              <Link to='/'>Main page</Link>
-            </h3>
-        <h3>Executive board</h3>
-        <GridList cellHeight={180} className={styles.gridList}>
-          <GridListTile key='Subheader' cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component='div'></ListSubheader>
-          </GridListTile>
-          {execBoardData.map(tile => (
-            <GridListTile key={tile.imgSrc}>
-              <img src={tile.imgSrc} alt={tile.name} />
-              <GridListTileBar
-                title={tile.name}
-                subtitle={<span>{tile.title}</span>}
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-    )
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 500,
+    height: 650
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)'
   }
+}))
+
+// export default class About extends React.Component {
+export default function TitlebarGridList() {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <h3>
+        <Link to='/'>Main page</Link>
+      </h3>
+      <GridList cellHeight={300} className={classes.gridList}>
+        <GridListTile key='Subheader' cols={4} style={{ height: 'auto' }}>
+          <ListSubheader component='div'>IEEE Executive Board</ListSubheader>
+        </GridListTile>
+        {teamNames.execBoardData.map(tile => (
+          <GridListTile key={tile.imgSrc}>
+            <img src={tile.imgSrc} alt={tile.name} />
+            <GridListTileBar
+              title={tile.name}
+              subtitle={<span>{tile.title}</span>}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+      <GridList cellHeight={300} className={classes.gridList}>
+        <GridListTile key='Subheader' cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component='div'>Website Masters</ListSubheader>
+        </GridListTile>
+        {teamNames.websiteMasterData.map(tile => (
+          <GridListTile key={tile.imgSrc}>
+            <img src={tile.imgSrc} alt={tile.name} />
+            <GridListTileBar
+              title={tile.name}
+              subtitle={<span>{tile.title}</span>}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  )
 }
--- src/about/teamNames.js
+++ src/about/teamNames.js
@@ -3,7 +3,11 @@ import Celine_Khoo from '../assets/Celine_Khoo.jpg'
 import Denise_Weinberger from '../assets/Denise_Weinberger.jpg'
 import Matt_Rantz from '../assets/Matt_Rantz.jpg'
 
-const execBoardData = [
+import Hoang_Nguyen from '../assets/Hoang_Nguyen.png'
+import Daniel_Erbynm from '../assets/Daniel_Erbynm.jpg'
+// Steven Khoa
+
+export const execBoardData = [
   {
     imgSrc: Joseph_Theoharakis,
     name: 'Joseph Theoharakis',
@@ -26,4 +30,38 @@ const execBoardData = [
   }
 ]
 
-export default execBoardData
+export const websiteMasterData = [
+  {
+    imgSrc: Hoang_Nguyen,
+    name: 'Hoang Nguyen'
+  },
+  {
+    imgSrc: Daniel_Erbynm,
+    name: 'Daniel Erbynm'
+  }
+]
+/*
+Tapasya Sharma
+Daniel Erbynm
+Hoang Nguyen
+Steven Kearns
+Jason R. Carrete
+Mridul Awasthi
+Shara Saketkhou
+Zach Matusen
+*/
+
+/*
+Names to add:
+
+Anuva Atandrila
+Aviroop Majumdar
+Gabby Dela Paz
+Har Patel
+Kristopher Lopez
+Saem Jeon
+Steven Lee
+Tapasya Sharma
+Viwing Zheng
+Yigit Alparslan
+*/
