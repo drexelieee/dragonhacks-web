f9f0a5d5c22d1379f8caf6ce4c9639a6d4256ba7
Steven Kearns <skearns@vgimfs.com>
2020-02-10 23:12:40 -0500

    Finally was able to make header conditional.

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
-// import { Link, IconButton, MenuItem, Menu } from '@material-ui/core'
-import { Link } from '@material-ui/core'
+import { Link, IconButton, MenuItem, Menu } from '@material-ui/core'
 
 import { makeStyles } from '@material-ui/styles'
-// import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
-// import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
 
 import logo from '../img/ICONS/Logo.svg'
 
@@ -33,22 +32,16 @@ const useStyles = makeStyles({
   }
 })
 
-export default function Header() {
-  // const Header = props => {
+// export default function Header() {
+const Header = props => {
   // Make component responsive to screen size
-  // const displayDropdown = () => {
-  //   if (isWidthUp('sm', props.width)) {
-  //     return true
-  //   }
-  //   return false
-  // }
-  // const [anchorEl, setAnchorEl] = React.useState(null)
-  // const openMenu = event => {
-  //   setAnchorEl(event.currentTarget)
-  // }
-  // const closeMenu = () => {
-  //   setAnchorEl(null)
-  // }
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const openMenu = event => {
+    setAnchorEl(event.currentTarget)
+  }
+  const closeMenu = () => {
+    setAnchorEl(null)
+  }
 
   const HomeLink = React.forwardRef((props, ref) => (
     <RouterLink innerRef={ref} {...props} />
@@ -62,71 +55,80 @@ export default function Header() {
   const AboutLink = React.forwardRef((props, ref) => (
     <RouterLink innerRef={ref} {...props} />
   ))
+
+  function ConditionalDropdown(props) {
+    let width = props.width
+    let anchorEl = props.anchorEl
+    if (isWidthUp('sm', width)) {
+      return (
+        <div>
+          <nav>
+            <ul className={classes.linkContainer}>
+              <li className={classes.link}>
+                <Link component={HomeLink} to='/'>
+                  Home
+                </Link>
+              </li>
+              <li className={classes.link}>
+                <Link component={OrganizerLink} to='/attend'>
+                  Organizer
+                </Link>
+              </li>
+              <li className={classes.link}>
+                <Link component={SponsorLink} to='/sponsor'>
+                  Sponsor
+                </Link>
+              </li>
+              <li className={classes.link}>
+                <Link component={AboutLink} to='/about'>
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <IconButton
+          aria-controls='simple-menu'
+          aria-haspopup='true'
+          onClick={openMenu}
+        >
+          <KeyboardArrowDownIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={closeMenu}
+        >
+          <Link component={HomeLink} to='/'>
+            <MenuItem>Home</MenuItem>
+          </Link>
+          <Link component={OrganizerLink} to='/attend'>
+            <MenuItem>Organizer</MenuItem>
+          </Link>
+          <Link component={SponsorLink} to='/sponsor'>
+            <MenuItem>Sponsor</MenuItem>
+          </Link>
+          <Link component={AboutLink} to='/about'>
+            <MenuItem>About</MenuItem>
+          </Link>
+        </Menu>
+      </div>
+    )
+  }
+
   const classes = useStyles()
   return (
     <header className={classes.root}>
       <img className={classes.logo} width={32} src={logo} alt='' />
       <h1 className={classes.heading}>DragonHacks</h1>
-      {/* <IconButton
-        aria-controls='simple-menu'
-        aria-haspopup='true'
-        onClick={openMenu}
-      >
-        <KeyboardArrowDownIcon />
-      </IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={closeMenu}
-      >
-        <MenuItem>
-          <Link component={HomeLink} to='/'>
-            Home
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link component={OrganizerLink} to='/attend'>
-            Organizer
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link component={SponsorLink} to='/sponsor'>
-            Sponsor
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link component={AboutLink} to='/about'>
-            About
-          </Link>
-        </MenuItem>
-      </Menu> */}
-      <nav>
-        <ul className={classes.linkContainer}>
-          <li className={classes.link}>
-            <Link component={HomeLink} to='/'>
-              Home
-            </Link>
-          </li>
-          <li className={classes.link}>
-            <Link component={OrganizerLink} to='/attend'>
-              Organizer
-            </Link>
-          </li>
-          <li className={classes.link}>
-            <Link component={SponsorLink} to='/sponsor'>
-              Sponsor
-            </Link>
-          </li>
-          <li className={classes.link}>
-            <Link component={AboutLink} to='/about'>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <ConditionalDropdown width={props.width} anchorEl={anchorEl} />
     </header>
   )
 }
 
-// export default withWidth()(Header)
+export default withWidth()(Header)
